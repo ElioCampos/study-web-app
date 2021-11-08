de080f2a91564e055909548f40440005ff2aeb38
ElioCampos <eliosalva91@gmail.com>
2021-11-07 21:05:13 -0500

    format router.dart

--- lib/router.dart
+++ lib/router.dart
@@ -2,11 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:study_web_app/pages/main_page.dart';
 import 'package:study_web_app/pages/navigator.dart';
 import 'package:study_web_app/pages/simulator_page.dart';
+import 'package:study_web_app/pages/teacher/make_evaluation_page.dart';
 import 'pages/login_page.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
-  // var id = settings.arguments as int;
-  // var userId = settings.arguments as int;
   switch (settings.name) {
     case 'main':
       return PageRouteBuilder(pageBuilder: (_, __, ___) => const MainPage());
@@ -18,8 +17,10 @@ Route<dynamic> generateRoute(RouteSettings settings) {
     case 'simulator':
       return PageRouteBuilder(
           pageBuilder: (_, __, ___) => const SimulatorPage());
-    
+    case 'makeExam':
+      return PageRouteBuilder(
+          pageBuilder: (_, __, ___) => const MakeEvaluationPage());
     default:
-      return MaterialPageRoute(builder: (context) => const MainPage());
+      return PageRouteBuilder(pageBuilder: (_, __, ___) => const MainPage());
   }
 }
